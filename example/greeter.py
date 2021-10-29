91613a4e50e0d5166e878b155956d8e23cd5186c
Peter Sulcs <Peter.Sulcs@gmail.com>
2021-10-29 12:07:01 -0700

    clean up

--- example/greeter.py
+++ example/greeter.py
@@ -1,16 +1,21 @@
 from typing import Optional
-from example import __version__
+
 import typer
 
+from example import __version__
+
 app = typer.Typer()
+
 
 @app.command("hello", help="say hello")
 def hello(name: Optional[str] = typer.Argument(None)) -> None:
     typer.echo(f"Hello, {name}, version {__version__}!")
 
+
 @app.command("bye", help="say goodbye")
 def bye(name: Optional[str] = typer.Argument(None)) -> None:
     typer.echo(f"Good bye, {name}!")
+
 
 def main():
     app()
