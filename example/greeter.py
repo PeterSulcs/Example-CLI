c1570ceb2e37ec4e19a8fc09e8409275c4cb5709
Peter Sulcs <Peter.Sulcs@gmail.com>
2021-10-29 13:12:24 -0700

    add gitignore add settings.json

--- example/greeter.py
+++ example/greeter.py
@@ -6,27 +6,31 @@ from example import __version__
 
 app = typer.Typer()
 
+
 def version_callback(value: bool):
     if value:
-        typer.echo(f"Awesome CLI Version: {__version__}")
+        typer.echo(f"Greeter CLI Version: {__version__}")
         raise typer.Exit()
+
 
 @app.command("hello", help="say hello")
 def hello(name: Optional[str] = typer.Argument(None)) -> None:
-    typer.echo(f"Hello, {name}, version {__version__}!")
+    typer.echo(f"Hello, {name}!")
 
 
 @app.command("bye", help="say goodbye")
 def bye(name: Optional[str] = typer.Argument(None)) -> None:
     typer.echo(f"Good bye, {name}!")
 
+
 @app.callback(invoke_without_command=True, no_args_is_help=True)
 def base(
-        version: Optional[bool] = typer.Option(
+    version: Optional[bool] = typer.Option(
         None, "--version", callback=version_callback, is_eager=True
-    ),
+    )
 ):
     pass
+
 
 def main():
     app()
